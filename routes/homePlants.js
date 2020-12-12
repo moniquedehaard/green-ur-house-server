32df24afed2e2fb15b34d84d73d67bb1b15a60bf
Monique de Haard <moniquedehaard@msn.com>
2020-12-12 19:51:54 +0100

    Edit plant

--- routes/homePlants.js
+++ routes/homePlants.js
@@ -8,7 +8,19 @@ const User = require("../models/User.model")
 const isLoggedIn = require("../middlewares/isLoggedIn");
 
 
-// Get all informatio n about homeplants for given user
+
+// Get all information about homeplant for given homeplant id
+router.get('/plants/:id', (req, res) => {
+    //console.log('ID',  req.params.id)
+    HomePlants
+        .findById(req.params.id)
+        .populate("species")
+        .then(foundPlant => {
+        res.json({foundPlant: foundPlant})
+    }).catch(err => console.log(err))
+})
+
+// Get all information about homeplants for given user
 router.get('/:id', (req, res, next) => {
     HomePlants
         .find({ user: req.params.id })
@@ -21,6 +33,38 @@ router.get('/:id', (req, res, next) => {
 
 // Post new plant
 router.post("/new", isLoggedIn, (req, res) => {
+    console.log(req.user)
+    const { plant, nickname, room, notes } = req.body
+    console.log(plant)
+
+    Plant
+        .findOne({ latinName: plant })
+        .then(plant => {
+            console.log('Found plant', plant)
+            HomePlants.create({
+                nickname: nickname,
+                species: plant._id,
+                room: room,
+                notes: notes,
+                user: req.user._id
+            }).then(createdPlant => {
+                console.log("NEW created homeplant", createdPlant)
+                // User updaten met plant ID
+                User.findByIdAndUpdate(req.user._id, {
+                    $addToSet: { homePlants: plant._id }
+                },
+                    { new: true }
+                ).then(updatedUser => {
+                    console.log('Updated user', updatedUser);
+                    res.json(updatedUser)
+                 })
+            })
+        })
+        .catch(err => console.log(err))
+})
+
+// Edit home plant
+router.post("/edit/:id", isLoggedIn, (req, res) => {
     console.log(req.user)
     const { plant, nickname, room, notes } = req.body
     console.log(plant)
